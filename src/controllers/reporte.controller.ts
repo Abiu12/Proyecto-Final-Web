f81f239026d612c0ecdf5ffb23bc233c5a0d70fe
Abiu <callejas195@gmail.com>
2022-12-04 14:16:30 -0600

    Future:Refactor elec y orden

--- src/controllers/reporte.controller.ts
+++ src/controllers/reporte.controller.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from "express";
+import { ElectrodomesticosModel } from "../models/electrodomesticos.model";
 import { OrdenTrabajoModel } from "../models/orden.trabajo.model";
 
 export async function viewReporte(req: Request, res: Response) {
   res.render("reportes/reporte-view");
 }
 export async function getData(req: Request, res: Response) {
-  const orden = await OrdenTrabajoModel.findAll({
+  const ordenes = await OrdenTrabajoModel.findAll({
     attributes: ["precio"],
     raw: true,
+  });
+
+  const electrodomesticos = await -ElectrodomesticosModel.findAll({
+    attributes: ["estado"],
+    raw: true,
+    where:  
   });
   res.status(200).json(orden);
 }
--- src/types/orden.trabajo.type.ts
+++ src/types/orden.trabajo.type.ts
@@ -2,6 +2,11 @@ export default interface OrdenTrabajoType {
   idOrden? :number;
   trabajo?: string;
   precio?: string;
+  fecha_entrada?: string;
+  fecha_salida?: string;
+  estado?: string;
+  garantia?: string;
+  observaciones?: string;
   idCliente?:number;
   idElectrodomestico?:number;
   
